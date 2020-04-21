3a9dcf5da031c1858b7fff1fe5d94aa329fd0b53
LaurenceDupont <LaurenceDupont@users.noreply.github.com>
2020-04-21 13:01:47 -0400

    feat: update daily ci enroll flow (#49) (#53)
    Co-authored-by: Laurence Dupont <ldupont@nuecho.com>

--- action-server/actions/daily_ci_enroll_form.py
+++ action-server/actions/daily_ci_enroll_form.py
@@ -6,14 +6,9 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
 
 FIRST_NAME_SLOT = "first_name"
-CONTACT_MODE = "contact_mode"
 PHONE_NUMBER_SLOT = "phone_number"
-EMAIL_SLOT = "email"
-AGE_GROUP_SLOT = "age_group"
 PRE_EXISTING_CONDITIONS_SLOT = "pre_existing_conditions"
-CHECKIN_TIME_SLOT = "checkin_time"
 
-EMAIL_REGEX = re.compile(r"\S+@\S+\.\S+")
 NOT_DIGIT_REGEX = re.compile(r"\D")
 
 
@@ -26,16 +21,11 @@ class DailyCiEnrollForm(FormAction):
     def required_slots(tracker: Tracker) -> List[Text]:
         """A list of required slots that the form has to fill"""
 
-        slots = [FIRST_NAME_SLOT, CONTACT_MODE]
-
-        contact_mode = tracker.get_slot(CONTACT_MODE)
-
-        if contact_mode == "phone":
-            slots.append(PHONE_NUMBER_SLOT)
-        elif contact_mode == "email":
-            slots.append(EMAIL_SLOT)
-
-        slots += [AGE_GROUP_SLOT, PRE_EXISTING_CONDITIONS_SLOT, CHECKIN_TIME_SLOT]
+        slots = [
+            FIRST_NAME_SLOT,
+            PHONE_NUMBER_SLOT,
+            PRE_EXISTING_CONDITIONS_SLOT,
+        ]
 
         return slots
 
@@ -43,7 +33,6 @@ class DailyCiEnrollForm(FormAction):
         return {
             FIRST_NAME_SLOT: self.from_text(),
             PHONE_NUMBER_SLOT: self.from_text(),
-            EMAIL_SLOT: self.from_text(),
             PRE_EXISTING_CONDITIONS_SLOT: [
                 self.from_intent(intent="affirm", value=True),
                 self.from_intent(intent="deny", value=False),
@@ -59,6 +48,7 @@ class DailyCiEnrollForm(FormAction):
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
         dispatcher.utter_message(template="utter_thanks_first_name", first_name=value)
+        dispatcher.utter_message(template="utter_text_message_checkin")
 
         return {FIRST_NAME_SLOT: value}
 
@@ -71,18 +61,10 @@ class DailyCiEnrollForm(FormAction):
     ) -> Dict[Text, Any]:
         phone_number = _get_phone_number(value)
 
+        if phone_number is not None:
+            dispatcher.utter_message(template="utter_acknowledge")
+
         return {PHONE_NUMBER_SLOT: phone_number}
-
-    def validate_email(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        email = _get_email(value)
-
-        return {EMAIL_SLOT: email}
 
     def validate_pre_existing_conditions(
         self,
@@ -98,21 +80,7 @@ class DailyCiEnrollForm(FormAction):
 
             slot_values[PRE_EXISTING_CONDITIONS_SLOT] = True
 
-        dispatcher.utter_message(template="utter_daily_ci_enroll_done")
-        dispatcher.utter_message(template="utter_daily_ci_enroll_follow_up")
-
         return slot_values
-
-    def validate_checkin_time(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        dispatcher.utter_message(template=f"utter_acknowledge_checkin_time_{value}")
-
-        return {CHECKIN_TIME_SLOT: value}
 
     def submit(
         self,
@@ -120,14 +88,11 @@ class DailyCiEnrollForm(FormAction):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict]:
+        dispatcher.utter_message(template="utter_daily_ci_answers_recorded")
+        dispatcher.utter_message(template="utter_daily_ci_enroll_done")
+        dispatcher.utter_message(template="utter_daily_ci_enroll_follow_up")
 
         return []
-
-
-def _get_email(text: Text) -> Optional[Text]:
-    match = EMAIL_REGEX.search(text)
-
-    return match.group() if match else None
 
 
 def _get_phone_number(text: Text) -> Optional[Text]:

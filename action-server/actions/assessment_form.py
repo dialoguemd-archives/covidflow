30558ea4ac425559280b92c556693dc592b410c9
Karine Dery <karine.dery@nuecho.com>
2020-04-27 13:31:35 -0400

    feat(flow): update assessment flows to 9a (#87)

--- action-server/actions/assessment_form.py
+++ action-server/actions/assessment_form.py
@@ -5,20 +5,9 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
 
-ASSESSMENT_TYPE_SLOT = "assessment_type"
-SEVERE_SYMPTOMS_SLOT = "severe_symptoms"
-MODERATE_SYMPTOMS_SLOT = "moderate_symptoms"
-HAS_FEVER_SLOT = "has_fever"
-PROVINCE_SLOT = "province"
-AGE_OVER_65_SLOT = "age_over_65"
-HAS_COUGH_SLOT = "has_cough"
-SYMPTOMS_SLOT = "symptoms"
-SELF_ASSESS_DONE_SLOT = "self_assess_done"
+from actions.assessment_common import AssessmentCommon, AssessmentSlots
 
-TESTED_POSITIVE = "tested_positive"
-SUSPECT = "suspect"
-CHECKIN_RETURN = "checkin_return"
-GET_ASSESSMENT = "get_assessment"
+CONTACT_RISK_SLOT = "has_contact_risk"
 
 
 class AssessmentForm(FormAction):
@@ -26,95 +15,85 @@ class AssessmentForm(FormAction):
 
         return "assessment_form"
 
-    ## Order corresponds to v8 entry and mild flows updated - may not match returning and tested_positive flows
     @staticmethod
     def required_slots(tracker: Tracker) -> List[Text]:
-        if tracker.get_slot(SEVERE_SYMPTOMS_SLOT):
-            return [ASSESSMENT_TYPE_SLOT, SEVERE_SYMPTOMS_SLOT]
-        if tracker.get_slot(MODERATE_SYMPTOMS_SLOT):
-            return [
-                ASSESSMENT_TYPE_SLOT,
-                SEVERE_SYMPTOMS_SLOT,
-                PROVINCE_SLOT,
-                AGE_OVER_65_SLOT,
-                HAS_FEVER_SLOT,
-                MODERATE_SYMPTOMS_SLOT,
-            ]
+        base_assessment_slots = AssessmentCommon.base_required_slots(tracker)
 
-        return [
-            ASSESSMENT_TYPE_SLOT,
-            SEVERE_SYMPTOMS_SLOT,
-            PROVINCE_SLOT,
-            AGE_OVER_65_SLOT,
-            HAS_FEVER_SLOT,
-            MODERATE_SYMPTOMS_SLOT,
-            HAS_COUGH_SLOT,
-        ]
+        # When we don't display self-isolation messages
+        if (
+            tracker.get_slot(AssessmentSlots.SEVERE_SYMPTOMS) is True
+            or tracker.get_slot(AssessmentSlots.TRAVEL) is False
+        ):
+            return base_assessment_slots
+
+        # conditional mild symptoms-contact-travel logic
+        if tracker.get_slot(AssessmentSlots.HAS_COUGH) is False:
+            base_assessment_slots.append(AssessmentSlots.CONTACT)
+        if tracker.get_slot(AssessmentSlots.CONTACT) is False:
+            base_assessment_slots.append(AssessmentSlots.TRAVEL)
+
+        base_assessment_slots.append(AssessmentSlots.LIVES_ALONE)
+        return base_assessment_slots
 
     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-        return {
-            ASSESSMENT_TYPE_SLOT: [
-                self.from_trigger_intent(intent=TESTED_POSITIVE, value=TESTED_POSITIVE),
-                self.from_trigger_intent(intent=SUSPECT, value=SUSPECT),
-                self.from_trigger_intent(intent=CHECKIN_RETURN, value=CHECKIN_RETURN),
-                self.from_trigger_intent(intent=GET_ASSESSMENT, value=GET_ASSESSMENT),
-            ],
-            AGE_OVER_65_SLOT: [
-                self.from_intent(intent="affirm", value=True),
-                self.from_intent(intent="deny", value=False),
-            ],
-            SEVERE_SYMPTOMS_SLOT: [
-                self.from_intent(intent="affirm", value=True),
-                self.from_intent(intent="deny", value=False),
-            ],
-            HAS_FEVER_SLOT: [
-                self.from_intent(intent="affirm", value=True),
-                self.from_intent(intent="deny", value=False),
-            ],
-            MODERATE_SYMPTOMS_SLOT: [
-                self.from_intent(intent="affirm", value=True),
-                self.from_intent(intent="deny", value=False),
-            ],
-            HAS_COUGH_SLOT: [
-                self.from_intent(intent="affirm", value=True),
-                self.from_intent(intent="deny", value=False),
-            ],
-        }
+        return AssessmentCommon.slot_mappings(self)
 
-    def validate_assessment_type(
+    def validate_moderate_symptoms(
         self,
         value: Text,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
+        if value is True:
+            dispatcher.utter_message(template="utter_symptoms_self_isolate")
 
-        if value == GET_ASSESSMENT:
-            return {ASSESSMENT_TYPE_SLOT: SUSPECT}
+        return {AssessmentSlots.MODERATE_SYMPTOMS: value}
 
-        if value == CHECKIN_RETURN:
-            dispatcher.utter_message(template="utter_returning_for_checkin")
-        elif value == TESTED_POSITIVE:
-            dispatcher.utter_message(template="utter_self_isolate_separate_room")
-            dispatcher.utter_message(template="utter_dont_leave_home")
-            dispatcher.utter_message(template="utter_deliver_food_medications")
-            dispatcher.utter_message(template="utter_home_assistance")
-            dispatcher.utter_message(template="utter_assess_symptoms")
-
-        return {ASSESSMENT_TYPE_SLOT: value}
-
-    def validate_has_fever(
+    def validate_has_cough(
         self,
         value: Text,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
-        if tracker.get_slot(ASSESSMENT_TYPE_SLOT) == CHECKIN_RETURN:
-            dispatcher.utter_message(template="utter_self_isolate_reminder")
-            dispatcher.utter_message(template="utter_home_assistance")
+        if value is True:
+            dispatcher.utter_message(template="utter_symptoms_self_isolate")
 
-        return {HAS_FEVER_SLOT: value}
+        return {AssessmentSlots.HAS_COUGH: value}
+
+    def validate_contact(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        if value is True:
+            dispatcher.utter_message(template="utter_contact_risk_self_isolate")
+
+        return {AssessmentSlots.CONTACT: value}
+
+    def validate_travel(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        if value is True:
+            dispatcher.utter_message(template="utter_contact_risk_self_isolate")
+
+        return {AssessmentSlots.TRAVEL: value}
+
+    def validate_lives_alone(
+        self,
+        value: bool,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        return AssessmentCommon.validate_lives_alone(value, dispatcher)
 
     def submit(
         self,
@@ -123,15 +102,12 @@ class AssessmentForm(FormAction):
         domain: Dict[Text, Any],
     ) -> List[Dict]:
 
-        symptoms_value = "none"
-        if tracker.get_slot(SEVERE_SYMPTOMS_SLOT):
-            symptoms_value = "severe"
-        elif tracker.get_slot(MODERATE_SYMPTOMS_SLOT):
-            symptoms_value = "moderate"
-        elif tracker.get_slot(HAS_COUGH_SLOT):
-            symptoms_value = "mild"
+        if (
+            tracker.get_slot(AssessmentSlots.CONTACT) is True
+            or tracker.get_slot(AssessmentSlots.TRAVEL) is True
+        ):
+            return [SlotSet(CONTACT_RISK_SLOT, True,)] + AssessmentCommon.submit(
+                self, dispatcher, tracker, domain
+            )
 
-        return [
-            SlotSet(SYMPTOMS_SLOT, symptoms_value),
-            SlotSet(SELF_ASSESS_DONE_SLOT, True),
-        ]
+        return AssessmentCommon.submit(self, dispatcher, tracker, domain)

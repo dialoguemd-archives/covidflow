e5d7d2cba09062d199da515ef5d676687d62613a
NetBKarine <63261085+NetBKarine@users.noreply.github.com>
2020-05-15 15:30:00 -0400

    feat(flow): severe symptoms and other mini fix (#257)
    Co-authored-by: Karine Dery <karine.dery@nuecho.com>

---
+++ action-server/covidflow/actions/action_greeting_messages.py
@@ -0,0 +1,24 @@
+from typing import Any, Dict, List, Text
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+from .lib.log_util import bind_logger
+
+
+class ActionGreetingMessages(Action):
+    def name(self) -> Text:
+        return "action_greeting_messages"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        bind_logger(tracker)
+        dispatcher.utter_message(template="utter_greet")
+        dispatcher.utter_message(template="utter_greet_warning")
+        dispatcher.utter_message(template="utter_ask_how_may_i_help")
+
+        return []

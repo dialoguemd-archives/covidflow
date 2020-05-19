2a864fd76910540d3bce95453c18ae113271c2fe
Mathieu Bergeron <mathieu.bergeron@nuecho.com>
2020-05-19 10:20:05 -0400

    fix: also wrapping RestInput

--- core/channels/__init__.py
+++ core/channels/__init__.py
@@ -1,18 +1,30 @@
 from structlog.contextvars import bind_contextvars, clear_contextvars
 
 from rasa.core.channels import SocketIOInput
+from rasa.core.channels.channel import RestInput
 
 from .setup_structlog import setup_structlog
 
 setup_structlog()
 
 
+def _create_on_new_message_wrapper(on_new_message):
+    # This wrapper function binds sender_id to contextvars before calling the original function
+    def on_new_message_wrapper(message):
+        clear_contextvars()
+        bind_contextvars(sender_id=message.sender_id)
+        return on_new_message(message)
+
+    return on_new_message_wrapper
+
+
 class WrappedSocketIOInput(SocketIOInput):
     def blueprint(self, on_new_message):
-        # This wrapper function binds sender_id to contextvars before calling the original function
-        def on_new_message_wrapper(message):
-            clear_contextvars()
-            bind_contextvars(sender_id=message.sender_id)
-            return on_new_message(message)
+        on_new_message_wrapper = _create_on_new_message_wrapper(on_new_message)
+        return super().blueprint(on_new_message_wrapper)
 
+
+class WrappedRestInput(RestInput):
+    def blueprint(self, on_new_message):
+        on_new_message_wrapper = _create_on_new_message_wrapper(on_new_message)
         return super().blueprint(on_new_message_wrapper)

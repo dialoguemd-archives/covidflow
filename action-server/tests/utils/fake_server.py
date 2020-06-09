15dc77c6117c4121fe8e8f6627fb961ef34664dc
Vincent Lavoie <vlavoie@nuecho.com>
2020-06-09 12:52:03 -0400

    fix: skipped async tests (#334)

--- action-server/tests/utils/fake_server.py
+++ action-server/tests/utils/fake_server.py
@@ -1,15 +1,20 @@
-from typing import Callable
+from typing import Callable, Optional
 
 from aiohttp import web
 from aiohttp.test_utils import TestServer
 
 
 class FakeServer:
-    def __init__(self, route: str, create_response: Callable[[], web.Response]):
+    def __init__(
+        self,
+        route: str,
+        create_response: Callable[[], web.Response],
+        port: Optional[int] = None,
+    ):
         self.app = web.Application()
         self.create_response = create_response
         self.app.router.add_routes([web.post(route, self.trigger_query)])
-        self.server = TestServer(self.app)
+        self.server = TestServer(self.app, port=port)
 
     async def start(self):
         await self.server.start_server()
--- action-server/tests/utils/test_phone_number_validation.py
+++ action-server/tests/utils/test_phone_number_validation.py
@@ -1,57 +1,90 @@
-from unittest import TestCase, skip
+import asyncio
+from typing import Callable
+from unittest import TestCase
 from unittest.mock import patch
 
-import pytest
+from aiohttp import web
 
 from covidflow.utils.phone_number_validation import (
     _generate_validation_code,
     send_validation_code,
 )
 
-# Async mocking problem:
-# See https://bugs.python.org/issue36996. It was solved, but not in Python 3.6.8,
-# thus some tests had to be skipped. Since we do not intend to change the python
-# version, no issue was create to fix this.
+from .fake_server import FakeServer
+
+CORE_SERVER = "covidflow.utils.phone_number_validation.CORE_SERVER_PREFIX"
+
+
+def create_api_success(data: dict = {}):
+    return lambda: web.json_response(data)
+
+
+def create_api_failure():
+    return lambda: web.HTTPBadRequest()
+
+
+def success_body_data(validation_code: str):
+    return {
+        "name": "send_validation_code",
+        "entities": {"validation_code": validation_code, "first_name": "Myname"},
+    }
 
 
 class PhoneNumberValidationTest(TestCase):
-    def test_generate_validation_code(self):
-        validation_code = _generate_validation_code()
-        self.assertEqual(len(validation_code), 4)
-        self.assertTrue(validation_code.isnumeric())
-
-    @pytest.mark.asyncio
-    @patch("covidflow.utils.phone_number_validation.ClientSession")
-    async def test_send_validation_code_555_number(self, mock_client_session):
-        validation_code = await send_validation_code("15145554321", "fr", "Myname")
-        self.assertEqual(validation_code, "4321")
-        mock_client_session.assert_not_called()
-
-    @skip("Async mocking problem")
-    @pytest.mark.asyncio
-    @patch("covidflow.utils.phone_number_validation.ClientSession")
-    async def test_send_validation_code_success(self, mock_client_session):
-        print(mock_client_session)
-        mock_client_session.return_value.post.return_value = {"status": 200}
-        validation_code = await send_validation_code("15148884321", "fr", "Myname")
-        mock_client_session.return_value.post.assert_called_once_with(
-            url="http://core-fr:8080/conversations/15148884321/trigger_intent",
-            params={"output_channel": "twilio"},
-            body={
-                "name": "send_validation_code",
-                "entities": {
-                    "validation_code": validation_code,
-                    "first_name": "Myname",
-                },
-            },
+    def _setUp(self, response: Callable[[], web.Response], number: str = ""):
+        self.loop = asyncio.new_event_loop()
+        self.server = FakeServer(
+            f"/conversations/{number}/trigger_intent", response, port=8080
         )
-        self.assertNotEqual(validation_code, "4321")
 
-    @skip("Async mocking problem")
-    @pytest.mark.asyncio
+        self.endpoint = patch(CORE_SERVER, "127.0.0.1")
+        self.endpoint.start()
+
+        self.loop.run_until_complete(self.server.start())
+
+    def test_generate_validation_code(self):
+        self._setUp(create_api_success(), "15145554321")
+
+        async def run():
+            validation_code = _generate_validation_code()
+            self.assertEqual(len(validation_code), 4)
+            self.assertTrue(validation_code.isnumeric())
+
+        self.run = run
+
     @patch("covidflow.utils.phone_number_validation.ClientSession")
-    async def test_send_validation_code_error(self, mock_client_session):
-        mock_client_session.return_value.post.return_value = {"status": 500}
-        validation_code = await send_validation_code("15148884321", "fr", "Myname")
+    def test_send_validation_code_555_number(self, mock_client_session):
+        self._setUp(create_api_success(), "15145554321")
 
-        self.assertIsNone(validation_code)
+        async def run():
+            validation_code = await send_validation_code("15145554321", "", "Myname")
+            self.assertEqual(validation_code, "4321")
+            mock_client_session.assert_not_called()
+
+        self.run = run
+
+    @patch("covidflow.utils.phone_number_validation.ClientSession")
+    def test_send_validation_code_success(self, mock_client_session):
+        self._setUp(create_api_success(success_body_data("1234")), "15148884321")
+
+        async def run():
+            validation_code = await send_validation_code("15148884321", "", "Myname")
+            self.assertIsNotNone(validation_code)
+            self.assertNotEqual(validation_code, "4321")
+
+        self.run = run
+
+    @patch("covidflow.utils.phone_number_validation.ClientSession")
+    def test_send_validation_code_error(self, mock_client_session):
+        self._setUp(create_api_failure(), "15148884321")
+
+        async def run():
+            validation_code = await send_validation_code("15148884321", "", "Myname")
+            self.assertIsNone(validation_code)
+
+        self.run = run
+
+    def tearDown(self):
+        self.loop.run_until_complete(self.run())
+        self.loop.run_until_complete(self.server.stop())
+        self.endpoint.stop()

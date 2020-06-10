be8de5fb749f38d100dab6f6cd2a98c16f6c302b
Mathieu Bergeron <mathieu.bergeron@nuecho.com>
2020-06-10 13:57:36 -0400

    fix: build

--- core/core/__init__.py
+++ core/core/__init__.py
@@ -1,3 +1,7 @@
-from .setup_structlog import setup_structlog
+# Running this in try/except to avoid error while training rasa models (structlog not found)
+try:
+    from .setup_structlog import setup_structlog
 
-setup_structlog()
+    setup_structlog()
+except:
+    pass
